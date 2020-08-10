d55d5b0d422e316db428907e33df0b28c73133ca
Tim <tsclay9@gmail.com>
2020-08-10 10:20:13 -0400

    setting up server

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+require('dotenv').config()
+
+const PORT = process.env.PORT || 5000;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/jre-api'
+
+const app = express()
+
+mongoose.connect(
+  MONGODB_URI,
+  {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  },
+  () => {
+    console.log(`🍃 Connected to MONGODB @ ${MONGODB_URI} 🍃`);
+  }
+)
+
+app.listen(PORT, () => {
+  console.log(`🚀 Listening on PORT:${PORT} 🚀`)
+})
