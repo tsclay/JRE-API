a4cf8c2913510d240503df1a213142d7671892b1
Tim <tsclay9@gmail.com>
2020-08-21 18:21:13 -0400

    episodes 1-299 have youtube links attached now

--- controllers/main.js
+++ controllers/main.js
@@ -2,8 +2,7 @@ const express = require('express')
 const moment = require('moment')
 const seed = require('../models/seed')
 const Episode = require('../models/Episodes')
-const FC = require('../FC')
-const youTubeLinks = require('../youtube.FC')
+const first = require('../youtube.First')
 
 const main = express.Router()
 
@@ -18,11 +17,66 @@ main.get('/seed', async (req, res) => {
 // Get all the episodes in reverse chronological order
 main.get('/all', (req, res) => {
   Episode.aggregate(
-    [{ $project: { _id: 0, __v: 0 } }, { $sort: { date: -1 } }],
+    [
+      {
+        $match: {
+          isFC: false,
+          isMMA: false,
+          isJRQE: false,
+          episode_id: { $lte: 299 }
+        }
+      },
+      { $project: { _id: 0, __v: 0 } },
+      { $sort: { date: 1 } }
+    ],
     (error, data) => {
+      // data.forEach((d) => {
+      //   console.log(d.episode_id)
+      // })
+
       return error ? res.json(error) : res.json(data)
     }
   )
+})
+
+main.get('/all-300-499', async (req, res) => {
+  try {
+    const data = await Episode.aggregate([
+      {
+        $match: {
+          isFC: false,
+          isMMA: false,
+          isJRQE: false,
+          episode_id: { $gte: 300, $lte: 499 }
+        }
+      },
+      { $project: { __v: 0 } },
+      { $sort: { date: 1 } }
+    ])
+
+    console.log(data.length, first.length)
+
+    // for (let i = 0; i < data.length; i++) {
+    //   const videoUrls = []
+    //   for (let j = 0; j < first.length; j++) {
+    //     if (data[i].episode_id === first[j].episode_id) {
+    //       videoUrls.push(first[j].links)
+    //       data[i].video_urls = videoUrls
+    //       // Episode.updateOne(
+    //       //   { _id: data[i]._id },
+    //       //   { video_urls: videoUrls },
+    //       //   (err, raw) => {
+    //       //     console.log('added videos!')
+    //       //   }
+    //       // )
+    //     }
+    //   }
+    // }
+
+    res.json(data)
+  } catch (error) {
+    console.log(error)
+  }
 })
 
 // Get one episode and send it to front for example of data and format
@@ -56,7 +110,7 @@ main.get('/fc', async (req, res) => {
 main.get('/mma', async (req, res) => {
   try {
     const data = await Episode.find({ isMMA: true }).sort({ date: -1 })
-    console.log(data.length)
+
     res.json(data)
   } catch (error) {
     console.log(error)
