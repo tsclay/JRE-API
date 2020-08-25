65a761f8c27e592f9600d1b6e7de9b12e84de38a
Tim <tsclay9@gmail.com>
2020-08-25 02:00:24 -0400

    create seed file for api key db

---
+++ models/pgSeed.sql
@@ -0,0 +1,18 @@
+DROP DATABASE IF EXISTS jre_keys;
+DROP USER IF EXISTS jamie;
+
+CREATE USER jamie WITH PASSWORD 'higherPrimate';
+
+CREATE DATABASE jre_keys;
+ALTER DATABASE jre_keys OWNER TO jamie;
+
+\c jre_keys;
+
+CREATE TABLE keys
+(id SERIAL, name VARCHAR(255), email TEXT, api_key VARCHAR(20));
+ALTER TABLE keys OWNER TO jamie;
+
+INSERT INTO keys (name, email, api_key) VALUES
+('Tim Clay', 'timclay@me.com', '14kewoi541acnsd'),
+('John Doe', 'johndoe@me.com', '109djqpoeir'),
+('Jane Smith', 'janesmith@me.com', 'mnzbclapor91824');
