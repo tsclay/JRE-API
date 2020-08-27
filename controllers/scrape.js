82d0a2a12e9773b3803086a0df1b418f1dd4ceb6
Tim <tsclay9@gmail.com>
2020-08-27 19:15:18 -0400

    can only access scrape route with auth key

--- controllers/scrape.js
+++ controllers/scrape.js
@@ -5,174 +5,182 @@ const cheerio = require('cheerio')
 const express = require('express')
 const puppeteer = require('puppeteer')
 const Episode = require('../models/Episodes')
+const authorizeScrape = require('../middleware/authorizeScrape')
+require('dotenv').config()
 
 const scraper = express.Router()
 
+const { SECRET } = process.env
+
 // Scrape the most recent podcast data, format it, and add it to database
-scraper.get('/api/scrape-recent', async (req, res) => {
-  const epNumRegEx = /#\d+/
-  const descriptionRegEx = /(?<=\s)(.*(?!$))/g
-  const dateRegEx = /\./g
-  const goods = []
-  let matchIndex = 0
+scraper.get(
+  '/api/scrape-recent',
+  authorizeScrape({ SECRET }),
+  async (req, res) => {
+    const epNumRegEx = /#\d+/
+    const descriptionRegEx = /(?<=\s)(.*(?!$))/g
+    const dateRegEx = /\./g
+    const goods = []
+    let matchIndex = 0
 
-  try {
-    // Scrape the first page of podcasts and grab the most recent podcast from database
-    const podcasts = await fetch('http://podcasts.joerogan.net/')
-    const data = await Episode.aggregate([
-      { $sort: { date: -1, episode_id: 1 } },
-      { $limit: 1 }
-    ])
+    try {
+      // Scrape the first page of podcasts and grab the most recent podcast from database
+      const podcasts = await fetch('http://podcasts.joerogan.net/')
+      const data = await Episode.aggregate([
+        { $sort: { date: -1, episode_id: 1 } },
+        { $limit: 1 }
+      ])
 
-    // Set the cheerio context to the HTML of the scraped page
-    const body = await podcasts.text()
-    const $ = cheerio.load(body)
+      // Set the cheerio context to the HTML of the scraped page
+      const body = await podcasts.text()
+      const $ = cheerio.load(body)
 
-    // Grab the description, date, and title of the podcasts
-    const content = $('.podcast-content')
-    const dates = $('div.podcast-date > h3')
-    const titles = $('a.ajax-permalink > h3')
-    const displayLinks = []
+      // Grab the description, date, and title of the podcasts
+      const content = $('.podcast-content')
+      const dates = $('div.podcast-date > h3')
+      const titles = $('a.ajax-permalink > h3')
+      const displayLinks = []
 
-    // If the lenghts of the above don't match, we got broken data; throw error
-    if (
-      !(
-        content.length === dates.length &&
-        content.length === titles.length &&
-        titles.length === dates.length
-      )
-    ) {
-      const error = new Error(
-        'The NodeLists are not the same length. Data cannot be properly aligned.'
-      )
-      throw error
-    }
-
-    // Grab the show page URLs for each episode and store for later use
-    $('div.podcast-details a.ajax-permalink:first-child').each((i, elem) => {
-      const raw = $(elem).attr('href')
-      displayLinks.push(raw)
-    })
-
-    console.log(displayLinks)
-
-    // Loop through description nodes and parse out the descriptions and episode numbers separately
-    content.each((i, elem) => {
-      const raw = $(elem).text()
-
-      const thisDescription = raw.match(descriptionRegEx)[0]
-      const thisEpNum = raw.match(epNumRegEx)[0]
-
-      // Create object that has those values as props
-      const obj = {
-        episode_id: Number(thisEpNum.slice(1)) || thisEpNum,
-        description: thisDescription || 'placeholder'
+      // If the lenghts of the above don't match, we got broken data; throw error
+      if (
+        !(
+          content.length === dates.length &&
+          content.length === titles.length &&
+          titles.length === dates.length
+        )
+      ) {
+        const error = new Error(
+          'The NodeLists are not the same length. Data cannot be properly aligned.'
+        )
+        throw error
       }
 
-      // if one of the scraped episodes matches the most recent episode from db, then set pointer for later db inserts
-      if (obj.episode_id === data[0].episode_id) {
-        matchIndex = i
-      }
-
-      // Push obj into placeholder
-      goods.push(obj)
-    })
-
-    // If after the above, the pointer is still 0, we are up-to-date; escape
-    if (matchIndex === 0) {
-      const error = new Error('Already updated')
-      throw error
-    }
-
-    // Same process, but with dates
-    dates.each((i, elem) => {
-      const raw = $(elem).text()
-      const thisDate = raw.replace(dateRegEx, '/')
-
-      goods[i].date = new Date(thisDate)
-    })
-
-    // Parse titles and guests, split guests into string array then set props onto each obj in the placeholder "goods"
-    titles.each((i, elem) => {
-      const raw = $(elem).text()
-      const thisTitle = `#${goods[i].episode_id} - ${raw}`
-      const theseGuests = raw.match(
-        /(?! )(?:([a-zA-Z\d \.]+)(?: ?)(?:"{0,}'? {0,})([a-zA-Z]+)(?:"{0,}'{0,} {0,}[a-zA-Z\d\.]+)?(?:(?:, )(?=Jr\.|Sr\.)..)?([a-zA-Z\d\.]))|(["a-zA-Z\d\.]+)/g
-      )
-      goods[i].guests = theseGuests
-      goods[i].title = thisTitle
-      goods[i].isMMA = raw.includes('JRE MMA Show') || false
-      goods[i].isFC = raw.includes('Fight Companion') || false
-      goods[i].isJRQE = false
-    })
-
-    // Automate a headless browser to go fetch the pages for each podcast link
-    // Then grab each podcast url and add "https" version to respective objs
-    const browser = await puppeteer.launch()
-    const page = await browser.newPage()
-    for (let i = 0; i < displayLinks.length; i++) {
-      await page.goto(displayLinks[i], { waitUntil: 'domcontentloaded' })
-      await page.waitForSelector('a.download-episode')
-      const pageData = await page.$eval('a.download-episode', (a) =>
-        a.getAttribute('href')
-      )
-      goods[i].podcast_url = await pageData.replace('http', 'https')
-    }
-
-    // Go to unofficial fan-site to scrape YouTube URLs
-    // Format them with proper "watch" queries and add to respective objs
-    await page.goto('https://www.jrepodcast.com/', {
-      waitUntil: 'domcontentloaded'
-    })
-    await page.waitForSelector('#site-footer-credit')
-    const pageData = await page.content()
-
-    const $youTube = await cheerio.load(pageData)
-
-    const links = []
-    $youTube('a[itemprop=url]').each((i, elem) => {
-      const oneLink = $(elem).attr('href')
-      links.push(oneLink)
-    })
-
-    for (let i = 0; i < displayLinks.length; i++) {
-      await page.goto(links[i], { waitUntil: 'domcontentloaded' })
-      await page.waitForSelector('#site-footer-credit')
-
-      const thisLink = await page.$eval('.ccb_single_video_player', (y) => {
-        return y.dataset.video_id
+      // Grab the show page URLs for each episode and store for later use
+      $('div.podcast-details a.ajax-permalink:first-child').each((i, elem) => {
+        const raw = $(elem).attr('href')
+        displayLinks.push(raw)
       })
-      await console.log(thisLink)
-      goods[i].video_urls = await [`https://youtube.com/watch?v=${thisLink}`]
+
+      console.log(displayLinks)
+
+      // Loop through description nodes and parse out the descriptions and episode numbers separately
+      content.each((i, elem) => {
+        const raw = $(elem).text()
+
+        const thisDescription = raw.match(descriptionRegEx)[0]
+        const thisEpNum = raw.match(epNumRegEx)[0]
+
+        // Create object that has those values as props
+        const obj = {
+          episode_id: Number(thisEpNum.slice(1)) || thisEpNum,
+          description: thisDescription || 'placeholder'
+        }
+
+        // if one of the scraped episodes matches the most recent episode from db, then set pointer for later db inserts
+        if (obj.episode_id === data[0].episode_id) {
+          matchIndex = i
+        }
+
+        // Push obj into placeholder
+        goods.push(obj)
+      })
+
+      // If after the above, the pointer is still 0, we are up-to-date; escape
+      if (matchIndex === 0) {
+        const error = new Error('Already updated')
+        throw error
+      }
+
+      // Same process, but with dates
+      dates.each((i, elem) => {
+        const raw = $(elem).text()
+        const thisDate = raw.replace(dateRegEx, '/')
+
+        goods[i].date = new Date(thisDate)
+      })
+
+      // Parse titles and guests, split guests into string array then set props onto each obj in the placeholder "goods"
+      titles.each((i, elem) => {
+        const raw = $(elem).text()
+        const thisTitle = `#${goods[i].episode_id} - ${raw}`
+        const theseGuests = raw.match(
+          /(?! )(?:([a-zA-Z\d \.]+)(?: ?)(?:"{0,}'? {0,})([a-zA-Z]+)(?:"{0,}'{0,} {0,}[a-zA-Z\d\.]+)?(?:(?:, )(?=Jr\.|Sr\.)..)?([a-zA-Z\d\.]))|(["a-zA-Z\d\.]+)/g
+        )
+        goods[i].guests = theseGuests
+        goods[i].title = thisTitle
+        goods[i].isMMA = raw.includes('JRE MMA Show') || false
+        goods[i].isFC = raw.includes('Fight Companion') || false
+        goods[i].isJRQE = false
+      })
+
+      // Automate a headless browser to go fetch the pages for each podcast link
+      // Then grab each podcast url and add "https" version to respective objs
+      const browser = await puppeteer.launch()
+      const page = await browser.newPage()
+      for (let i = 0; i < displayLinks.length; i++) {
+        await page.goto(displayLinks[i], { waitUntil: 'domcontentloaded' })
+        await page.waitForSelector('a.download-episode')
+        const pageData = await page.$eval('a.download-episode', (a) =>
+          a.getAttribute('href')
+        )
+        goods[i].podcast_url = await pageData.replace('http', 'https')
+      }
+
+      // Go to unofficial fan-site to scrape YouTube URLs
+      // Format them with proper "watch" queries and add to respective objs
+      await page.goto('https://www.jrepodcast.com/', {
+        waitUntil: 'domcontentloaded'
+      })
+      await page.waitForSelector('#site-footer-credit')
+      const pageData = await page.content()
+
+      const $youTube = await cheerio.load(pageData)
+
+      const links = []
+      $youTube('a[itemprop=url]').each((i, elem) => {
+        const oneLink = $(elem).attr('href')
+        links.push(oneLink)
+      })
+
+      for (let i = 0; i < displayLinks.length; i++) {
+        await page.goto(links[i], { waitUntil: 'domcontentloaded' })
+        await page.waitForSelector('#site-footer-credit')
+
+        const thisLink = await page.$eval('.ccb_single_video_player', (y) => {
+          return y.dataset.video_id
+        })
+        await console.log(thisLink)
+        goods[i].video_urls = await [`https://youtube.com/watch?v=${thisLink}`]
+      }
+
+      // Add the more recent episode objs to database
+      for (let i = matchIndex - 1; i >= 0; i--) {
+        console.log(goods[i])
+        Episode.create(goods[i])
+      }
+
+      // For debugging
+      // for (let i = goods.length - 1; i >= 0; i--) {
+      //   // console.log(goods[i])
+      //   Episode.create(goods[i])
+      // }
+
+      // Close puppeteer browser instance
+      await browser.close()
+    } catch (error) {
+      console.log(error)
+    } finally {
+      // res.json(goods)
+      // Fresh db call for all docs after all scraping is done
+      setTimeout(() => {
+        Episode.aggregate([
+          { $sort: { date: -1 } },
+          { $limit: 10 }
+        ]).then((data) => res.send(data))
+      }, 500)
     }
-
-    // Add the more recent episode objs to database
-    for (let i = matchIndex - 1; i >= 0; i--) {
-      console.log(goods[i])
-      Episode.create(goods[i])
-    }
-
-    // For debugging
-    // for (let i = goods.length - 1; i >= 0; i--) {
-    //   // console.log(goods[i])
-    //   Episode.create(goods[i])
-    // }
-
-    // Close puppeteer browser instance
-    await browser.close()
-  } catch (error) {
-    console.log(error)
-  } finally {
-    // res.json(goods)
-    // Fresh db call for all docs after all scraping is done
-    setTimeout(() => {
-      Episode.aggregate([
-        { $sort: { date: -1 } },
-        { $limit: 10 }
-      ]).then((data) => res.send(data))
-    }, 500)
   }
-})
+)
 
 scraper.get('/api/scrape-links', async (req, res) => {
   const epNumRegEx = /#\d+/
