2ee3aebe803d87b3bce93645973d2f40afc438e1
Tim <tsclay9@gmail.com>
2020-08-27 19:15:40 -0400

    removed unneeded code

--- middleware/verifyKey.js
+++ middleware/verifyKey.js
@@ -13,19 +13,6 @@ const verifyKey = (options) => {
     } catch (error) {
       return next(error)
     }
-
-    // pool.query(
-    //   'SELECT * FROM keys WHERE api_key = $1',
-    //   [apiKey],
-    //   (err, result) => {
-    //     // console.log('here is result', result)
-    //     if (result.rows[0] === undefined) {
-    //       const error = new Error('Invalid API Key. Access to resource denied.')
-    //       return next(error)
-    //     }
-    //     return next()
-    //   }
-    // )
   }
 }
 
