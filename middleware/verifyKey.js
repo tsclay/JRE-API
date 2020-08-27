ee4e273f581dd3834d9193594cdb973daef1e9cd
Tim <tsclay9@gmail.com>
2020-08-27 00:30:47 -0400

    refactored to work with wrapper class

--- middleware/verifyKey.js
+++ middleware/verifyKey.js
@@ -1,20 +1,31 @@
 const verifyKey = (options) => {
-  const { pool } = options
-  return (req, res, next) => {
+  const { Keys } = options
+  return async (req, res, next) => {
     const apiKey = req.get('X-API-KEY')
     console.log('the apiKey ', apiKey)
-    pool.query(
-      'SELECT * FROM keys WHERE api_key = $1',
-      [apiKey],
-      (err, result) => {
-        // console.log('here is result', result)
-        if (result.rows[0] === undefined) {
-          const error = new Error('Invalid API Key. Access to resource denied.')
-          return next(error)
-        }
-        return next()
+    try {
+      const result = await Keys.selectOne('keys', 'api_key', apiKey)
+      console.log(result)
+      if (!result) {
+        throw new Error('Invalid API Key. Access to resource denied.')
       }
-    )
+      return next()
+    } catch (error) {
+      return next(error)
+    }
+
+    // pool.query(
+    //   'SELECT * FROM keys WHERE api_key = $1',
+    //   [apiKey],
+    //   (err, result) => {
+    //     // console.log('here is result', result)
+    //     if (result.rows[0] === undefined) {
+    //       const error = new Error('Invalid API Key. Access to resource denied.')
+    //       return next(error)
+    //     }
+    //     return next()
+    //   }
+    // )
   }
 }
 
