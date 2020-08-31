acc4c515c25fae28ce8554f6b3984fe5ffb7b7e4
Tim Clay <tsclay9@gmail.com>
2020-08-31 11:42:32 -0400

    debug

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express()
 app.use(express.json())
 app.use(
   cors({
-    origin: 'https://jre-api.vercel.app/'
+    origin: 'https://jre-api.vercel.app'
   })
 )
 app.use('/api/v1', mainController)
